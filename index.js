71839fe335611dcbede27ceb151ea5eac97e5102
22680208 <22680208@cuautla.tecnm.mx>
2026-02-20 13:10:40 -0600

    Configuración de Node.js

---
+++ index.js
@@ -0,0 +1,26 @@
+import express from "express";
+import cors from "cors";
+import morgan from "morgan";
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+app.use(morgan("dev"));
+
+// Routes
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+
+// Error handling
+app.use((err, req, res, next) => {
+    console.error(err.stack);
+    res.status(500).send("Something broke!");
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
