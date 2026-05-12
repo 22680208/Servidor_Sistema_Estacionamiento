78048947eae6e23924c84813fa187066ea6bffcc
Zilum17 <Espejel950@gmail.com>
2026-05-12 15:20:12 -0600

    config mqtts

--- src/config/mqtt.js
+++ src/config/mqtt.js
@@ -1,15 +1,19 @@
 import mqtt from 'mqtt';
 
-const MQTT_URL = process.env.MQTT_URL || 'mqtt://localhost:1883';
+const protocol = process.env.MQTT_PROTOCOL;
+const host = process.env.MQTT_HOST;
+const port = Number(process.env.MQTT_PORT || 1883);
 
 const options = {
   clientId: `node_server_${Math.random().toString(16).slice(3)}`,
   clean: true,
   connectTimeout: 4000,
   reconnectPeriod: 1000,
+  username: process.env.MQTT_USERNAME || undefined,
+  password: process.env.MQTT_PASSWORD || undefined,
 };
 
-const mqttClient = mqtt.connect(MQTT_URL, options);
+const mqttClient = mqtt.connect(`${protocol}://${host}:${port}`, options);
 
 mqttClient.on('connect', () => {
   console.log('[MQTT] Conectado al Broker');
