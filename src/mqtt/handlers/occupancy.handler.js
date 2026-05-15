6da84c69fd1f54df195af983821df65490d1198b
Zilum17 <Espejel950@gmail.com>
2026-05-15 12:07:59 -0600

    v 1.0

--- src/mqtt/handlers/occupancy.handler.js
+++ src/mqtt/handlers/occupancy.handler.js
@@ -1,5 +1,21 @@
+import { publishRealtimeEvent } from '../../services/realtime-pubsub.service.js';
+import { parkingSummary } from '../../utils/structureForResponse.js';
+import Place from '../../models/Place.js';
 export async function handleOccupancyMessage(payload) {
   const data = JSON.parse(payload);
+  const place = await Place.find({number: data.placeNumber});
+  if (!place) return;
+  place.state = data.occupied ? 'ocupado' : 'disponible'
 
-  console.log('Occupancy recibida:', data);
+  await place.save();
+
+  const dashboardData = await parkingSummary();
+  await publishRealtimeEvent({
+    channel: 'dashboard',
+    event: 'dashboard.updated',
+    payload: {
+      message: 'reserva auto-cancelada',
+      data: dashboardData,
+    },
+  });
 }
